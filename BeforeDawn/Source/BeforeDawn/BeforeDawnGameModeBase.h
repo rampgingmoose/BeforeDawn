87a4123e68925987abaec3c48b4853f3d5d0f3c7
rampgingmoose <76532218+rampgingmoose@users.noreply.github.com>
2024-03-05 16:35:32 -0500

    Initial Project Creation
    -Test Map created
    -Player Character Imported

---
+++ BeforeDawn/Source/BeforeDawn/BeforeDawnGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BeforeDawnGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BEFOREDAWN_API ABeforeDawnGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
